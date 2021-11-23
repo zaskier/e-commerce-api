4d931ea6cb4027480ff983b99c9b579a02549504
Wojciech Iskierka <zaskier@gmail.com>
2021-11-23 09:39:44 +0100

    imported typeORM userRespository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsersController } from './controllers/users.controller'
 import { User } from './entities/user.entity'
+import { UserRepository } from './models/user.repository'
 import { UsersService } from './services/users.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), TypeOrmModule.forFeature([UserRepository])],
   controllers: [UsersController],
   providers: [UsersService],
 })
