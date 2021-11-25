914daddb6b830406c683155a05db1094a0098b35
Wojciech Iskierka <zaskier@gmail.com>
2021-11-25 11:20:34 +0100

    added CRUD operations with PostrgeSQL

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -3,28 +3,43 @@ import { CreateUserDto } from '../controllers/dto/create-user.dto'
 import { UpdateUserDto } from '../controllers/dto/update-user.dto'
 import { InjectRepository } from '@nestjs/typeorm'
 import { UserRepository } from '../models/user.repository'
-
+const upperCamelCase = require('uppercamelcase')
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {}
 
   async createNewUser(createUserDto: CreateUserDto) {
+    createUserDto.email = createUserDto.email.toLowerCase()
+    createUserDto.name = upperCamelCase(createUserDto.name)
+    createUserDto.surname = upperCamelCase(createUserDto.surname)
     return await this.userRepository.save(createUserDto)
   }
 
   findAll() {
-    return `This action returns all users`
+    return this.userRepository.find()
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`
+    return this.userRepository.findByIds([id])
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`
+    if (updateUserDto.email) {
+      updateUserDto.email = updateUserDto.email.toLowerCase()
+    }
+    if (updateUserDto.name) {
+      updateUserDto.name = upperCamelCase(updateUserDto.name)
+    }
+    if (updateUserDto.surname) {
+      updateUserDto.surname = upperCamelCase(updateUserDto.surname)
+    }
+
+    let updateEditedAt: Date = new Date()
+    updateUserDto.editedAt = updateEditedAt
+    return this.userRepository.update(id, updateUserDto)
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`
+    return this.userRepository.delete(id)
   }
 }
