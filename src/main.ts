3f8721cae712d569a064e5762f7939e414922b42
Wojciech Iskierka <zaskier@gmail.com>
2021-11-29 14:26:13 +0100

    added session configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as session from 'express-session'
+import * as passport from 'passport'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(
+    session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 }, //cookie expiring in ms
+    }),
+  )
+  app.use(passport.initialize())
+  app.use(passport.session())
   app.setGlobalPrefix('api')
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true, //todo validate with according to cooding standards
+      transform: true, //todo later set validation for specific Pipes
     }),
   )
   await app.listen(process.env.PORT || 3000)
