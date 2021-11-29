6dbf9ebd7ad3a2dac11ac355909ac09d9a96ccd5
Wojciech Iskierka <zaskier@gmail.com>
2021-11-29 09:32:28 +0100

    added mocked logIn

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -3,11 +3,34 @@ import { CreateUserDto } from '../controllers/dto/create-user.dto'
 import { UpdateUserDto } from '../controllers/dto/update-user.dto'
 import { InjectRepository } from '@nestjs/typeorm'
 import { UserRepository } from '../models/user.repository'
+import { type } from 'os'
 const upperCamelCase = require('uppercamelcase')
+
+export type User = {
+  id: number
+  name: string
+  username: string
+  password: string
+}
+
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {}
+  // mocked Users
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'Marius',
+      username: 'Marius7',
+      password: 'pass1',
+    },
+    { id: 2, name: 'Philip', username: 'Phil1', password: 'pass2' },
+  ]
 
+  async logInUser(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username) //createDB object
+  }
+  //
   async createNewUser(createUserDto: CreateUserDto) {
     createUserDto.email = createUserDto.email.toLowerCase()
     createUserDto.name = upperCamelCase(createUserDto.name)
@@ -18,6 +41,12 @@ export class UsersService {
   findAll() {
     return this.userRepository.find()
   }
+
+  // findAdmins() {
+  //   return this.userRepository.find()
+
+  //   return `This action returns all admin users`
+  // }
 
   findOne(id: number) {
     return this.userRepository.findByIds([id])
