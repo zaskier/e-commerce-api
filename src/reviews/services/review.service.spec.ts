50b8dc8f11ed83ee08bd8b6f060672bb610dbc38
Wojciech Iskierka <zaskier@gmail.com>
2021-12-03 08:22:06 +0100

    init service first test

--- src/reviews/services/review.service.spec.ts
+++ src/reviews/services/review.service.spec.ts
@@ -1,18 +1,23 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ReviewService } from './review.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { ReviewController } from '../controllers/review.controller'
+import { ReviewService } from './review.service'
 
 describe('ReviewService', () => {
-  let service: ReviewService;
-
+  let service: ReviewService
+  const mockReviewService = {}
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers: [ReviewController],
       providers: [ReviewService],
-    }).compile();
+    })
+      .overrideProvider(ReviewService)
+      .useValue(mockReviewService)
+      .compile()
 
-    service = module.get<ReviewService>(ReviewService);
-  });
+    service = module.get<ReviewService>(ReviewService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
