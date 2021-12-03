1d4bbfbfdf6db61d8b445fbe35dbfa44e6ae3170
Wojciech Iskierka <zaskier@gmail.com>
2021-12-03 13:39:32 +0100

    removed default route implementatio used for development

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-  getUsersData(): { name: string } {
-    return { name: 'Server is active' }
+  logger: Logger
+
+  constructor() {
+    this.logger = new Logger()
   }
 }
