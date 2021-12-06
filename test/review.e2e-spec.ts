90af1971a5aed5bc8961b09228018572ca2cd370
Wojciech Iskierka <zaskier@gmail.com>
2021-12-06 09:29:30 +0100

    added post and and post type validation

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { INestApplication } from '@nestjs/common'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
 import * as request from 'supertest'
 import { getRepositoryToken } from '@nestjs/typeorm'
 import { ReviewPostEntity } from '../src/reviews/models/post.entity'
@@ -7,8 +7,11 @@ import { ReviewModule } from '../src/reviews/review.module'
 
 describe('ReviewController (e2e)', () => {
   let app: INestApplication
+  const mockReviews = { id: 1, body: '7/10' }
   const mockReviewRepository = {
-    find: jest.fn(),
+    find: jest.fn().mockResolvedValue(mockReviews),
+    create: jest.fn().mockImplementation(dto => dto),
+    save: jest.fn().mockImplementation(ReviewPostEntity => Promise.resolve({ id: Date.now(), ...ReviewPostEntity })),
   } //mock db connection // mock db migrations for schemas
 
   beforeEach(async () => {
@@ -20,10 +23,47 @@ describe('ReviewController (e2e)', () => {
       .compile()
 
     app = moduleFixture.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe())
     await app.init()
   })
 
   it('/review (GET)', () => {
-    return request(app.getHttpServer()).get('/review').expect(200)
+    return request(app.getHttpServer())
+      .get('/review')
+
+      .expect('Content-Type', /json/)
+
+      .expect(200)
+
+      .expect(mockReviews)
+  })
+
+  it('/review (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/review')
+
+      .send({ body: '7/10' })
+
+      .expect('Content-Type', /json/)
+
+      .expect(201)
+
+      .then(response => {
+        expect(response.body).toEqual({
+          id: expect.any(Number),
+          body: '7/10',
+        })
+      })
+  })
+
+  it('/review (POST) --> 400 on validation error', () => {
+    return request(app.getHttpServer())
+      .post('/review')
+
+      .send({ body: 333 })
+
+      .expect('Content-Type', /json/)
+
+      .expect(400)
   })
 })
