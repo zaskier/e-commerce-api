7f0c85bcfa87db99c12739ed074fd68af3b4c42c
Wojciech Iskierka <zaskier@gmail.com>
2021-12-06 14:48:33 +0100

    autentication moved to controller from CRUD operations

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -8,12 +8,13 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 const upperCamelCase = require('uppercamelcase')
 
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
   @ApiBody({ type: CreateUserDto })
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @ApiCreatedResponse({ description: 'User Registration' })
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.createNewUser(createUserDto)
@@ -21,26 +22,26 @@ export class UsersController {
 
   @Get()
   @ApiOkResponse({ description: 'Get all users' })
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   findAll() {
     return this.usersService.findAll()
   }
 
   @Get(':id')
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id)
   }
 
   @Patch(':id')
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @ApiBody({ type: UpdateUserDto })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.updateUser(+id, updateUserDto)
   }
 
   @Delete(':id')
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id)
   }
