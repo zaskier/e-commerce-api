e44ed1af58a0bbdac8b37ac55c2dde4b919619ef
Wojciech Iskierka <zaskier@gmail.com>
2021-12-06 14:44:14 +0100

    added authorisation for endpoints

--- src/reviews/controllers/review.controller.ts
+++ src/reviews/controllers/review.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common'
 import { ReviewService } from '../services/review.service'
 import { ReviewPost } from '../models/post.model'
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() reviewPost: ReviewPost) {
     return this.reviewService.create(reviewPost)
   }
@@ -16,11 +18,13 @@ export class ReviewController {
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   updateComment(@Param('id') id: number, @Body() reviewPost: ReviewPost) {
     return this.reviewService.updateComment(+id, reviewPost)
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   delete(@Param('id') id: number) {
     return this.reviewService.deletePost(id)
   }
--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,11 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common'
-import { strictEqual } from 'assert'
-//import { UsersService } from 'src/users/services/users.service'
-import { BeforeInsert } from 'typeorm'
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards } from '@nestjs/common'
 import { UsersService } from '../services/users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { ApiCreatedResponse, ApiOkResponse, ApiBody } from '@nestjs/swagger'
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 
 const upperCamelCase = require('uppercamelcase')
 
@@ -15,7 +13,7 @@ export class UsersController {
 
   @Post()
   @ApiBody({ type: CreateUserDto })
-  // todo @ApiUnauthorizedResponse({ description: 'Unauthorised Access' })
+  @UseGuards(JwtAuthGuard)
   @ApiCreatedResponse({ description: 'User Registration' })
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.createNewUser(createUserDto)
@@ -23,22 +21,26 @@ export class UsersController {
 
   @Get()
   @ApiOkResponse({ description: 'Get all users' })
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.usersService.findAll()
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id)
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   @ApiBody({ type: UpdateUserDto })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.updateUser(+id, updateUserDto)
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id)
   }
