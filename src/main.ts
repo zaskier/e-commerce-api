daaa0776bf26fc03527e737349b009c96c0f79d5
Wojciech Iskierka <zaskier@gmail.com>
2021-12-06 11:35:04 +0100

    added swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as session from 'express-session'
 import * as passport from 'passport'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -12,6 +13,18 @@ async function bootstrap() {
       transform: true, //todo later set validation for specific Pipes
     }),
   )
+  const options = new DocumentBuilder()
+
+    .setTitle('E-commerce API')
+
+    .setDescription('e-commerce API')
+
+    .setVersion('1.0.0')
+
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('', app, document)
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
