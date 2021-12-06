77ba3b1651e3507e8992845d86ac1aa0cd127ba4
Wojciech Iskierka <zaskier@gmail.com>
2021-12-06 08:40:10 +0100

    added review repository

--- src/reviews/review.module.ts
+++ src/reviews/review.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { ReviewService } from './services/review.service';
-import { ReviewController } from './controllers/review.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ReviewPostEntity } from './models/post.entity';
+import { Module } from '@nestjs/common'
+import { ReviewService } from './services/review.service'
+import { ReviewController } from './controllers/review.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ReviewPostEntity } from './models/post.entity'
+import { ReviewRepository } from './models/reviews.repository'
 
 @Module({
   imports: [TypeOrmModule.forFeature([ReviewPostEntity])],
