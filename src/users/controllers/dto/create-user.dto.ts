5162864be15fd11821ec0f1183b7ac03b1fa6c22
Wojciech Iskierka <zaskier@gmail.com>
2021-12-07 10:41:26 +0100

    added enum validation in DTO

--- src/users/controllers/dto/create-user.dto.ts
+++ src/users/controllers/dto/create-user.dto.ts
@@ -1,7 +1,8 @@
-import { IsEmail, Length, IsNumber, IsString, IsNotEmpty } from 'class-validator'
+import { IsEmail, Length, IsNumber, IsIn, IsString, IsNotEmpty } from 'class-validator'
 import { RolesEnum } from 'src/users/models/user.model'
 import { BeforeInsert } from 'typeorm'
 import { ApiProperty, ApiCreatedResponse, ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger'
+import { isInt16Array } from 'util/types'
 
 export class CreateUserDto {
   @IsEmail()
@@ -20,6 +21,7 @@ export class CreateUserDto {
   surname: string
 
   @IsString()
+  @IsIn(['admin', 'user'])
   @ApiProperty({ type: String, description: 'role it can be admin or user' })
   role: RolesEnum
 
