78ba2d42346a872912cc38e9f584ec4d72fbe9de
Wojciech Iskierka <zaskier@gmail.com>
2021-12-07 10:24:25 +0100

    added swagger body for POST, PUT, PATCH added findOne Endpoint

--- src/reviews/controllers/review.controller.ts
+++ src/reviews/controllers/review.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post, Put, ParseIntPipe, UseGuards } from '@nestjs/common'
 import { ReviewService } from '../services/review.service'
 import { ReviewPost } from '../models/post.model'
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
+import { ApiBody, ApiTags } from '@nestjs/swagger'
 
 @Controller('review')
+@ApiTags('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
   @Post()
   @UseGuards(JwtAuthGuard)
+  @ApiBody({ type: ReviewPost })
   create(@Body() reviewPost: ReviewPost) {
     return this.reviewService.create(reviewPost)
   }
@@ -17,7 +20,13 @@ export class ReviewController {
     return this.reviewService.findAllPosts()
   }
 
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.reviewService.findOne(id)
+  }
+
   @Put(':id')
+  @ApiBody({ type: ReviewPost })
   @UseGuards(JwtAuthGuard)
   updateComment(@Param('id') id: number, @Body() reviewPost: ReviewPost) {
     return this.reviewService.updateComment(+id, reviewPost)
