ebe258d42d240155385f714814a31e2d82f008a9
Wojciech Iskierka <zaskier@gmail.com>
2021-12-07 14:13:52 +0100

    changed file names and added editedAt and rating

---
+++ src/reviews/models/review.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Index, Entity, PrimaryGeneratedColumn, BaseEntity } from 'typeorm'
+
+@Entity('review')
+export class Review extends BaseEntity {
+  @Index({ unique: true })
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ default: '' })
+  review: string
+
+  @Column()
+  rating: number
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  editedAt: Date
+
+  // @Column()
+  // email: string  //to do add email of peron posting this request
+}
---
+++ src/reviews/models/review.model.ts
@@ -0,0 +1,19 @@
+import { Max, Min, IsInt, IsString, IsNotEmpty } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+
+export class ReviewPost {
+  id?: number
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, description: 'review content' })
+  review: string
+  @IsInt()
+  @IsNotEmpty()
+  @Min(1)
+  @Max(5)
+  @ApiProperty({ type: String, description: 'rating from 1 to 5 stars' })
+  rating: number
+  createdAt?: Date
+  editedAt?: Date
+  email?: string
+}
