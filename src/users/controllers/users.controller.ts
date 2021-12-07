c46ca1837c8d4e3de64d2196be937718dda9a6ea
Wojciech Iskierka <zaskier@gmail.com>
2021-12-07 10:44:22 +0100

    Apitags moved from CRUD operations to controller

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGua
 import { UsersService } from '../services/users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
-import { ApiCreatedResponse, ApiOkResponse, ApiBody } from '@nestjs/swagger'
+import { ApiCreatedResponse, ApiOkResponse, ApiBody, ApiTags } from '@nestjs/swagger'
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 
 const upperCamelCase = require('uppercamelcase')
 
 @Controller('users')
+@ApiTags('users')
 @UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -22,26 +23,22 @@ export class UsersController {
 
   @Get()
   @ApiOkResponse({ description: 'Get all users' })
-  // @UseGuards(JwtAuthGuard)
   findAll() {
     return this.usersService.findAll()
   }
 
   @Get(':id')
-  // @UseGuards(JwtAuthGuard)
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id)
   }
 
   @Patch(':id')
-  // @UseGuards(JwtAuthGuard)
   @ApiBody({ type: UpdateUserDto })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.updateUser(+id, updateUserDto)
   }
 
   @Delete(':id')
-  // @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id)
   }
