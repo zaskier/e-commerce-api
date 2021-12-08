ecea4a76b53b6aa7e9901fc5cd2f5a72d8e8f122
Wojciech Iskierka <zaskier@gmail.com>
2021-12-08 10:27:32 +0100

    removed unused method

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -32,11 +32,6 @@ export class User extends BaseEntity {
     default: RolesEnum.User,
   })
   role: RolesEnum
-  ///todo Use or remove
-  async validatePassword(password: string): Promise<boolean> {
-    const hash = await bcrypt.hash(password, this.salt)
-    return hash === this.password
-  }
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date
