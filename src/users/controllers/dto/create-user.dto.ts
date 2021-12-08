ff629f2fe8f5f407cadbe05dcf293788121e9f2c
Wojciech Iskierka <zaskier@gmail.com>
2021-12-08 10:23:21 +0100

    removed unused function, changed formating

--- src/users/controllers/dto/create-user.dto.ts
+++ src/users/controllers/dto/create-user.dto.ts
@@ -1,38 +1,54 @@
-import { IsEmail, Length, IsNumber, IsIn, IsString, IsNotEmpty } from 'class-validator'
+import { IsEmail, Length, IsIn, IsString, IsNotEmpty } from 'class-validator'
 import { RolesEnum } from 'src/users/models/user.model'
-import { BeforeInsert } from 'typeorm'
-import { ApiProperty, ApiCreatedResponse, ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger'
-import { isInt16Array } from 'util/types'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class CreateUserDto {
   @IsEmail()
   @IsString()
-  @ApiProperty({ type: String, description: 'email' })
+  @ApiProperty({
+    type: String,
+    description: 'email',
+  })
   email: string
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty({ type: String, description: 'name' })
+  @ApiProperty({
+    type: String,
+    description: 'name',
+  })
   name: string
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty({ type: String, description: 'surname' })
+  @ApiProperty({
+    type: String,
+    description: 'surname',
+  })
   surname: string
 
   @IsString()
   @IsIn(['admin', 'user'])
-  @ApiProperty({ type: String, description: 'role it can be admin or user' })
+  @ApiProperty({
+    type: String,
+    description: 'role it can be admin or user',
+  })
   role: RolesEnum
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty({ type: String, description: 'phone number' })
+  @ApiProperty({
+    type: String,
+    description: 'phone number',
+  })
   phoneNumber: string
 
   @IsString()
   @Length(8, 256)
-  @ApiProperty({ type: String, description: 'password' })
+  @ApiProperty({
+    type: String,
+    description: 'password',
+  })
   password: string
 
   salt: string
--- src/users/controllers/dto/update-user.dto.ts
+++ src/users/controllers/dto/update-user.dto.ts
@@ -1,44 +1,61 @@
 import { PartialType } from '@nestjs/mapped-types'
-import { IsEmail, IsDate, IsOptional, Length, IsNumber, IsString, IsNotEmpty } from 'class-validator'
+import { IsEmail, IsOptional, Length, IsString, IsNotEmpty } from 'class-validator'
 import { RolesEnum } from 'src/users/models/user.model'
-//import { RolesEnum } from 'src/users/models/user.model'
 import { CreateUserDto } from './create-user.dto'
-import { ApiProperty, ApiCreatedResponse, ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   readonly id: number
   @IsOptional()
   @IsEmail()
-  @ApiProperty({ type: String, description: 'email' })
+  @ApiProperty({
+    type: String,
+    description: 'email',
+  })
   email?: string
 
   @IsOptional()
   @IsString()
   @IsNotEmpty()
-  @ApiProperty({ type: String, description: 'email' })
+  @ApiProperty({
+    type: String,
+    description: 'email',
+  })
   name?: string
 
   @IsOptional()
   @IsString()
   @IsNotEmpty()
-  @ApiProperty({ type: String, description: 'email' })
+  @ApiProperty({
+    type: String,
+    description: 'email',
+  })
   surname?: string
 
   @IsOptional()
   @IsString()
-  @ApiProperty({ type: String, description: 'role it can be admin or user' })
+  @ApiProperty({
+    type: String,
+    description: 'role it can be admin or user',
+  })
   role?: RolesEnum
 
   @IsOptional()
   @IsString()
   @IsNotEmpty()
-  @ApiProperty({ type: String, description: 'email' })
+  @ApiProperty({
+    type: String,
+    description: 'email',
+  })
   phoneNumber?: string
 
   @IsOptional()
   @IsString()
   @Length(8, 256)
-  @ApiProperty({ type: String, description: 'email' })
+  @ApiProperty({
+    type: String,
+    description: 'email',
+  })
   password?: string
 
   @IsOptional()
