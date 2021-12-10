f207fb2093e6070783123c422998b684636f895b
Wojciech Iskierka <zaskier@gmail.com>
2021-12-10 08:58:27 +0100

    added new Guard for admin authentication

---
+++ src/auth/guards/jwt-auth-admin.guard.ts
@@ -0,0 +1,18 @@
+import { ExecutionContext, HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+
+@Injectable()
+export class JwtAuthAdminGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context)
+  }
+
+  handleRequest(err, user, info) {
+    console.log(user)
+    // You can throw an exception based on either "info" or "err" arguments
+    if (err || !user || user.role != 'admin') {
+      throw err || new HttpException('Forbidden', HttpStatus.FORBIDDEN)
+    }
+    return user
+  }
+}
